fbada75b5cb621bc05e2a18df4b4c8be0c82b94f
unknown <vilvabharathi@gmail.com>
2024-07-30 15:54:57 +0530

    secondnewcode

---
+++ src/test/java/com/example/lap/LapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
